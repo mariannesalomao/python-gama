337b635343d3be69318bbc30bbdb3b1f3898c097
Marianne Salomao <mariannesalomaodeoliveira@hotmail.com>
2021-09-18 14:48:28 -0300

    Finalização do CRUD modelo

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Carros(models.Model):
+  modelo = models.CharField(max_length=150)
+  marca = models.CharField(max_length=100)
+  ano = models.IntegerField()
